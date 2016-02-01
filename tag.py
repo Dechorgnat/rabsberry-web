b1798fce211f5e93fac6ba1c4fe6a9309e4cc2dc
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-02-01 12:21:04 +0100

    add mongodb and store new rfid

--- tag.py
+++ tag.py
@@ -2,6 +2,17 @@ import subprocess
 from StringIO import StringIO
 from Log import Log
 
+from database import db
+
+def get_rfid_info(rfid_id):
+    rfid = db.rfid.find_one({"id": rfid_id})
+    if rfid:
+        return rfid
+    else:
+        rfid = {"id":rfid,"desc":"","action_in":"","action_out":""}
+        db.rfid.insert_one(rfid)
+        return rfid
+
 def manage_event(event):
     action = event['action']
     actor_id = event['actor_id']
@@ -9,9 +20,12 @@ def manage_event(event):
     if rfid_id =='0':
         Log.info("RFID reader "+ actor_id+ " is "+ action)
     else:
+        rfid = get_rfid_info(rfid_id)
+
         Log.info("Event from RFID reader "+ actor_id+ ": tag "+ rfid_id+ " get "+ action)
         if action == 'IN':
 	    Log.info("trying to read hour")
+            # TODO use config
             args = ['/home/pi/Rabsberry/rabsberry-core/plugins/clock/read_hour.py']
             p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output, error = p.communicate()
