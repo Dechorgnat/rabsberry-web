401cb83433ac3a391e3127afe3788b8d8b4f324c
Dechorgnat <dechorgnata@gmail.com>
2016-02-07 09:47:35 +0100

    refactoring + nginx

--- api/api.py
+++ api/api.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, base_dir)
+
 from flask import Flask
 from flask import request
-
 import json
-
 import tag
-
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/noroute')
+def hello():
+    return 'no route'
+
+@app.route('/api')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! in WSGI mode vraiment!!!!'
 
 @app.route('/api/event', methods=['POST'])
 def event():
@@ -24,4 +31,4 @@ def event():
 
 
 if __name__ == '__main__':
-    app.run(port="4321")
+    app.run()
