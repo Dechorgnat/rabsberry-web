03fb83982bfcd328625ce2ec7154739e2bd254c6
Alain Dechorgnat <dechorgnata@gmail.com>
2016-03-27 10:06:21 +0200

    Add navbar

---
+++ app/js/commons.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alain on 27/03/16.
+ */
+function HeaderController($scope, $location)
+{
+    $scope.isActive = function (viewLocation) {
+
+        return $location.path().indexOf(viewLocation) >= 0;
+        //return viewLocation === $location.path();
+    };
+}
