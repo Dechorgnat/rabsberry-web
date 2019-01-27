edaa0324ccc93d7cea2796a7bb212ed6201c4068
Dechorgnat <azaz1234>
2019-01-27 11:46:57 +0100

    prepare new leg management

--- api/leds.py
+++ api/leds.py
@@ -7,23 +7,11 @@ from core.tools.config import getConfig
 conf = getConfig()
 CORE_ROOT = conf['CORE_ROOT']
 
-import serial
-from time import sleep
 
 def manage_event(event):
     command = event['command']
-    ser = serial.Serial ("/dev/ttyAMA0")    #Open named port
-    ser.baudrate = 1200                     #Set baud rate to 9600
+    # action = event['action']
+    # actor_id = event['actor_id']
+    print event
 
-    i = 0
-    while True:
-        ser.write(command)                    #Send
-        #ser.write('\n')                    #Send
-        i=i+1
-        response = ser.read(2)
-        if response == "OK" or i==10:
-            break;
-        response = ''
-        sleep(0.5)
-    ser.close()
-    return response 
+    return "OK"
