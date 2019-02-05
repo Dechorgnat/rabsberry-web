63fb9cdabef9f22d8ec826d1fc1980469e8bcf8f
Dechorgnat <rance2001>
2019-02-05 21:29:33 +0100

    debug top button

--- api/button.py
+++ api/button.py
@@ -2,8 +2,10 @@
 
 import subprocess
 from Log import Log
-
+import json;
 from core.tools.config import getConfig
+import paho.mqtt.client as paho
+
 conf = getConfig()
 CORE_ROOT = conf['CORE_ROOT']
 
@@ -13,6 +15,19 @@ def manage_event(event):
     Log.info("Event from "+ actor_id+ ": action "+ action)
     if action == 'SHORT_PRESSED':
         Log.info("make R2D2 sream")
+        # init mqtt connection and subscribe
+        broker="127.0.0.1"
+        client = paho.Client("show_weather")  # create client
+        client.connect(broker)  # connect
+        message = {"command": "pattern", "pattern": "google"}
+        client.publish("leds",json.dumps(message))
+        print json.dumps(message)
+        
+        message = {"command": "step", "ear": "both", "pos":17, "dir":"forward"}
+        client.publish("ears",json.dumps(message))
+        print json.dumps(message)
+        
+
         args = [CORE_ROOT+'/functions/play_audio.py',
                 CORE_ROOT+'/resources/mp3/star_wars/r2d2/r2d2-squeaks5.mp3']
         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
