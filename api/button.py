07ce6dfde82540cfd930d65d075b45041a7017b6
Dechorgnat <dechorgnata@gmail.com>
2019-02-08 19:09:31 +0100

    add log to api/button.py

--- api/button.py
+++ api/button.py
@@ -14,26 +14,31 @@ def manage_event(event):
     actor_id = event['actor_id']
     Log.info("Event from "+ actor_id+ ": action "+ action)
     if action == 'SHORT_PRESSED':
-        Log.info("make R2D2 sream")
-        # init mqtt connection and subscribe
+        # init mqtt connection
         broker="127.0.0.1"
         client = paho.Client("show_weather")  # create client
         client.connect(broker)  # connect
+
+        # start new leds pettern
         message = {"command": "pattern", "pattern": "google"}
         client.publish("leds",json.dumps(message))
-        print json.dumps(message)
+        Log.info( json.dumps(message))
         
+        # make ears horizontals
         message = {"command": "goto", "ear": "both", "pos":13, "dir":"forward"}
         client.publish("ears",json.dumps(message))
-        print json.dumps(message)
+        Log.info(json.dumps(message))
         
-
+        # play a sound
         args = [CORE_ROOT+'/functions/play_audio.py',
                 CORE_ROOT+'/resources/mp3/star_wars/r2d2/r2d2-squeaks5.mp3']
+        Log.info("play a R2D2 sound")
         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = p.communicate()
         if p.returncode != 0:
+            Log.error("failed to play a R2D2 sound")
             raise subprocess.CalledProcessError(p.returncode, "can't play sound", error)
+        Log.info("succed to play a R2D2 sound " + output)
         return output
 
     return 'OK'
